a4c0e3cc06aedb59d5f0a73e49d363617752ef87
wang.zhuowei <wang.zhuowe@foxmail.com>
2020-06-17 15:12:43 +0800

    [cpp]在opengl项目中增加了面剔除的ut

--- src/cpp/opengl/src/advance_opengl/cullface_ut.cc
+++ src/cpp/opengl/src/advance_opengl/cullface_ut.cc
@@ -1,8 +1,8 @@
-// working_ut.cc -
+// cullface_ut.cc -
 // Version: 1.0
 // Author: Wang Zhuowei wang.zhuowei@foxmail.com
 // Copyright: (c) wang.zhuowei@foxmail.com All rights reserved.
-// Last Change: 2020 Jun 16
+// Last Change: 2020 Jun 17
 // License: GPL.v3
 
 #include <iostream>
@@ -78,7 +78,7 @@ static void scroll_callback(GLFWwindow* window, double x, double y)
     camera.ProcessScroll(y);
 }
 
-class WOKUT : public testing::Test {
+class CUFUT : public testing::Test {
   public:
     static void SetUpTestCase();
     static void TearDownTestCase() {}
@@ -86,7 +86,7 @@ class WOKUT : public testing::Test {
     void TearDown() override {}
 };
 
-void WOKUT::SetUpTestCase() {
+void CUFUT::SetUpTestCase() {
     // glfw: initialize and configure
     // ------------------------------
     glfwInit();
@@ -101,7 +101,7 @@ void WOKUT::SetUpTestCase() {
 
 }
 
-TEST_F(WOKUT, Hello) {
+TEST_F(CUFUT, HelloCullFace) {
     // glfw window creation
     // --------------------
     GLFWwindow* window = glfwCreateWindow(gldef::SCR_WIDTH, gldef::SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
